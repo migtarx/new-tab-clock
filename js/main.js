633bc7bae53a7c02a6cbdeda3d81cc77f5aa9a46
Miguel Puerta <migtarx@gmail.com>
2022-03-18 21:34:32 +0100

    fix: Refactored code and full lang selection support. Removed test files

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
-const langData = [['Buenos días','Buenas tardes','Buenas noches'],['Egun on','Arratsalde on','Gau on'],['Good morning','Good afternoon','Good evening']]
-let clockEl = document.getElementById("clockDiv");
-let msg = document.getElementById("welcome-msg");
+const langData = [['Nueva configuración','Buenos días','Buenas tardes','Buenas noches'],['Konfigurazio berria','Egun on','Arratsalde on','Gau on'],['New Configuration','Good morning','Good afternoon','Good evening']]
+var clockEl = document.getElementById("clockDiv");
+var msg = document.getElementById("welcome-msg");
 checkFirstTime()
+document.getElementById("lowerleft").textContent = langData[localStorage.getItem("lang")][0];
 
 function getClockTime() {
-    let date = new Date();
+    var date = new Date();
 
-    let hr = date.getHours();
-    let min = date.getMinutes();
+    var hr = date.getHours();
+    var min = date.getMinutes();
 
     hr = ("0" + hr).slice(-2);
     min = ("0" + min).slice(-2);
 
     function getDayZone() {
         if (hr < 12) {
-            return langData[localStorage.getItem("lang")][0];
-        } else if (hr < 18) {
             return langData[localStorage.getItem("lang")][1];
-        } else {
+        } else if (hr < 18) {
             return langData[localStorage.getItem("lang")][2];
+        } else {
+            return langData[localStorage.getItem("lang")][3];
         }
     }
 
